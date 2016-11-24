a703ac514d115d5a87972580e4e558affc9b1ec7
Charlie Parkinson <charlesparkinson14@gmail.com>
2016-11-23 22:21:21 -0800

    Committed Problem 19-21. Problem 21 has not been finished.

---
+++ Chapter04/Problem19-21/src/main.cpp
@@ -0,0 +1,102 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 22, 2016
+ *      Author: CPark
+ */
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//incomplete, working on problem 21
+
+int main(){
+	vector<int> scores;
+	vector<string> names;
+	int score;
+	string name;
+	cout<<"Please input a name and a score.\n";
+	cin>>name>>score;
+	for(unsigned int num=0;num<=names.size()||num==0;num++){
+		if(num!=0&&name==names[num-1]){
+			cout<<"You have already used this name. Please input another.\n";
+			num=0;
+		}
+		else if(num==names.size()){
+			if(name=="noName"&&score==0){
+				for(num=0;num<names.size();num++){
+					if(num<=names.size()){
+						cout<<names[num]<<" "<<scores[num]<<"\n";
+					}
+					else{
+						break;
+					}
+				}
+				break;
+			}
+			else{
+				names.push_back(name);
+				scores.push_back(score);
+				cout<<"Would you like to input a new name or view previous ones?\n";
+				while(10>1){
+					cout<<"Type 'previous' or 'new'.\n";
+					string check;
+					cin>>check;
+					if(check=="previous"){
+						cout<<"Would you like to input a name or a score?\n";
+						string check2;
+						cin>>check2;
+						while(2>1){
+							if(check2=="name"){
+								cout<<"Please input the name.\n";
+								cin>>name;
+								num=0;
+								for(num=0;num<names.size();num++){
+									if(name==names[num]){
+										cout<<scores[num];
+										break;
+									}
+									else if(num==names.size()){
+										cout<<"Name not found.\n";
+										break;
+									}
+								}
+								break;
+							}
+							else if(check2=="score"){
+								cout<<"Please input the score.\n";
+								cin>>score;
+								for(num=0;num<scores.size();num++){
+									if(score==scores[num]){
+										cout<<names[num]<<"\n";
+										vector<string> scoreCheck;
+										scoreCheck.push_back(names[num]);
+									}
+									// else if(num==scores.size()&&scoreCheck.size()==0){
+
+									//}
+								}
+								break;
+							}
+							else{
+								cout<<"Plese type 'name' or 'score'.\n";
+							}
+						}
+					}
+					else if(check=="new"){
+						break;
+					}
+					else{
+						cout<<"Please type 'previous' or 'new'.\n";
+					}
+					break;
+				}
+				break;
+			}
+		}
+		cin>>name>>score;
+	}
+}
+
